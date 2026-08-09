3b5515a529a0201fcb7cb3fe98bf4b046e42c156
Kane Stephens <kane_stephens@protonmail.com>
2026-08-09 09:36:36 +0100

    completed cors and javescript

--- Resume/visitorcounter.js
+++ Resume/visitorcounter.js
@@ -1,8 +1,9 @@
-let count = 0
-const api = "https://gwgcmdxf1f.execute-api.eu-west-2.amazonaws.com"
+const api = "https://gwgcmdxf1f.execute-api.eu-west-2.amazonaws.com/counter-stage/visitors"
 
 async function updateCounter() {
-    const response = await fetch(api);
+    const response = await fetch(api,{
+        method: "POST"
+    });
     const data = await response.json();
 
     document.getElementById("visitor-count").textContent = data.count;
